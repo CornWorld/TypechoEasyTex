86807901be97343570be7684ad7c77b2780c7357
CornWorld <cw@had.ink>
2022-03-25 10:47:52 +0800

    [Feat] Add support for KaTex

---
+++ Plugin.php
@@ -0,0 +1,64 @@
+<?php
+
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Widget\Options;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+/**
+ * TypechoEasyTex
+ *
+ * @package TypechoEasyTex
+ * @author CornWorld
+ * @version 0.1
+ * @link https://cornworld.com/TypechoEasyTex
+ */
+class TypechoEasyTex_Plugin implements Typecho_Plugin_Interface
+{
+    public static function activate() {
+        Typecho_Plugin::factory('Widget_Archive')->header = array(__CLASS__, 'header');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'footer');
+        Typecho_Plugin::factory('admin/write-post.php')->content = array(__CLASS__, 'header');
+        Typecho_Plugin::factory('admin/write-post.php')->bottom = array(__CLASS__, 'footer');
+    }
+
+    public static function deactivate() {}
+
+    public static function config(Typecho_Widget_Helper_Form $form) {
+        $url=new Typecho_Widget_Helper_Form_Element_Text('url',
+		NULL, 'pluginUrl', _t('Url'));
+		$form->addInput($url);
+    }
+    
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+
+    public static function header() {
+        $dir=Helper::options()->plugin('TypechoEasyTex')->url;
+        if(empty($dir) || $dir=='pluginUrl') $dir=Helper::options()->pluginUrl.'/TypechoEasyTex';
+        echo <<<HTML
+        <link href="{$dir}/katex/katex.min.css" rel="stylesheet">
+        <link href="{$dir}/katex/contrib/copy-tex.css" rel="stylesheet">
+        <style>.ketex{ font-size: 1.1em; text-indent: 0; text-rendering:0; }</style>
+        HTML;
+    }
+    
+    public static function footer() {
+        $dir=Helper::options()->plugin('TypechoEasyTex')->url;
+        if(empty($dir) || $dir=='pluginUrl') $dir=Helper::options()->pluginUrl.'/TypechoEasyTex';
+        echo <<<HTML
+        <script defer src="{$dir}/katex/katex.min.js"></script>
+        <script src="{$dir}/katex/contrib/copy-tex.js"></script>
+        <script defer src="{$dir}/katex/contrib/auto-render.min.js" onload="renderMathInElement(
+            document.getElementsByClassName('post-content')[0],{
+                delimiters: [ {left: '$$', right: '$$', display: true}, {left: '$', right: '$', display: false} ],
+                macros: { '\\ge': '\\geqslant','\\le': '\\leqslant','\\geq': '\\geqslant','\\leq': '\\leqslant'}
+            }
+        );"></script>
+        HTML;
+    }
+
+}
